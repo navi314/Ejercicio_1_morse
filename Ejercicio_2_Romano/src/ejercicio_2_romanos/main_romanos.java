10c3ccea8beabd3b0b5ec4e9339549a7efb494c2
navi314 <darkripper63@gmail.com>
2020-11-07 13:06:21 -0600

    Salir numeros romanos
    Se le dio la opcion al usuario de salir del programa

--- Ejercicio_2_Romano/src/ejercicio_2_romanos/main_romanos.java
+++ Ejercicio_2_Romano/src/ejercicio_2_romanos/main_romanos.java
@@ -15,12 +15,25 @@ public class main_romanos {
 		while(ciclo) {
 			System.out.println(" ============================================ ");
 			System.out.println(" Numeros Romanos");
-			System.out.println(" Ingresa el numero a convertir");
+			System.out.println(" Ingresa el numero a convertir (Este numero debera de estar en el rango del 1 al 1000)");
 			System.out.println(" ============================================ ");
 
 			try {
 				userInput = Integer.parseInt( reader.readLine().toUpperCase() );
-				System.out.println("Resultado: "+convertir.intToRoman(userInput));
+				if(userInput >= 1 && userInput <=1000) {
+					System.out.println(" ============================================ ");
+					System.out.println(" Resultado: "+convertir.intToRoman(userInput));
+					System.out.println(" 1) Continuar");
+					System.out.println(" 2) Salir");
+					System.out.println(" ============================================ ");
+					userInput = Integer.parseInt( reader.readLine().toUpperCase() );
+					if(userInput == 2) {
+						ciclo=false;
+						System.out.println("El programa termino");
+					}
+				}else {
+					System.out.println("El numero esta fuera del rango");
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch(NumberFormatException e) {
